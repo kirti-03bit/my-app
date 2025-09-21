7da975bfc26bd7970b065b62d36f7cbe84fdd5dd
Kirti <nalbalekirti@gmail.com>
2025-09-21 20:17:28 +0530

    preparing for deployment

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,35 @@
-
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
 
-const inter=Inter({subsets:["latin"]});
-
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "App",
-  description: "one stop Finance platform",
+  title: "Welth",
+  description: "One stop Finance Platform",
 };
-
+const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={'$(inter.className'}>
-       {/* header */}
-      <Header />
-      
-      <main className="min-h-screen">{children}</main>
-      <Toaster/>
-      <Toaster richColors/>
-      
-       {/* footer */}
-        <footer className= "bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center
-          text-gray-600">
-            <p>made with love by Roadsider</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by RoadsideCoder</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
